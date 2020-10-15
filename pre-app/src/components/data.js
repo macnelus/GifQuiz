903d2d1b2c95aa4d6fdb5d25f14643dcf1c2f3b5
Pedro Martin <pataruco@gmail.com>
2020-10-15 09:32:58 +0100

    Delete pre app

--- pre-app/src/components/data.js
+++
@@ -1,9 +0,0 @@
-
-const url = '';
-
- const getGiphyData = async () => {
-  const response = await fetch(`${url}`);
-  const data = await response.json();
-  return data;
- };
-export default getGiphyData;
